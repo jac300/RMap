bba80eb233c0ed5500dc5c344887e79f0089e8ed
Jennifer Clark <jac300@nyu.edu>
2017-05-04 15:39:30 -0700

    setup

---
+++ RMap/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  RMap
+//
+//  Created by Jennifer Clark on 5/4/17.
+//  Copyright © 2017 Jennifer Clark. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+}
+
